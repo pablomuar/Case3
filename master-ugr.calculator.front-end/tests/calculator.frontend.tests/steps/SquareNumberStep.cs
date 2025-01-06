5c529bdeb0f891213060529a9c0882e1fd3f8497
Diego Corral Fernández <corralfernandezdiego@gmail.com>
2025-01-06 17:35:04 +0100

    Agregación de la raiz cuadrada en la rama front-end

---
+++ master-ugr.calculator.front-end/tests/calculator.frontend.tests/steps/SquareNumberStep.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace calculator.lib.test.steps
+{
+    [Binding]
+    public class SquareRootSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
+        public SquareRootSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"a number (.*)")]
+        public void GivenANumber(double number)
+        {
+            _scenarioContext.Add("number", number);
+        }
+
+        [When("I calculate the square root")]
+        public void ICalculateTheSquareRoot()
+        {
+            var number = _scenarioContext.Get<double>("number");
+            var squareRoot = Math.Sqrt(number);
+            _scenarioContext.Add("squareRoot", squareRoot);
+        }
+
+        [Then("the result should be (.*)")]
+        public void TheResultShouldBe(double expected)
+        {
+            var squareRoot = _scenarioContext.Get<double>("squareRoot");
+            Assert.Equal(expected, squareRoot, precision: 2);
+        }
+    }
+}
